6009d0dd9aa441a82235dec2586363f12c28820c
foxieze <jonah@rowlinson.eu>
2024-08-21 23:37:12 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function ToggleNavbar() {
+    var hamburger = document.querySelector(".hamburger");
+    hamburger.classList.toggle("is-active");
+
+    var mobileMenu = document.querySelector(".mobileMenu");
+    mobileMenu.classList.toggle("active")
+    
+}
